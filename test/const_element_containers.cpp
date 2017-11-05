2853f4fb0bffa9065d2525ec40f241ce7c4d4c88
Peter Dimov <pdimov@pdimov.com>
2017-11-05 04:08:17 +0200

    Fix test; explicit instantiations are only valid at namespace scope

--- test/const_element_containers.cpp
+++ test/const_element_containers.cpp
@@ -10,68 +10,36 @@
 //
 
 #include <boost/ptr_container/ptr_container.hpp>
-#include <boost/test/unit_test.hpp>
 
-template< class T >
-void test_instantiations()
-{
-    // force instantiation of all members
-    template class
-    boost::ptr_array<T,42>;
+// force instantiation of all members
 
-    template class
-    boost::ptr_deque<T>;
+template class boost::ptr_array<const int, 42>;
+template class boost::ptr_array<boost::nullable<const int>, 42>;
 
-    template class
-    boost::ptr_list<T>;
-    
-    template class
-    boost::ptr_map<int,T>;
+template class boost::ptr_deque<const int>;
+template class boost::ptr_deque< boost::nullable<const int> >;
 
-    template class
-    boost::ptr_vector<T>;
+template class boost::ptr_list<const int>;
+template class boost::ptr_list< boost::nullable<const int> >;
 
-    //@todo problem with constructor forwarding
-    //template class
-    //boost::ptr_unordered_map<int,T>;
-    
-    // @todo: there seems to be some problems with
-    //        argument passing in circular_buffer
-    //boost::ptr_circular_buffer<T> buffer(32);
-    //buffer.push_back( new int(42)  );   
-}
+template class boost::ptr_map<int, const int>;
+template class boost::ptr_map<int, boost::nullable<const int> >;
 
+template class boost::ptr_vector<const int>;
+template class boost::ptr_vector< boost::nullable<const int> >;
 
+//@todo problem with constructor forwarding
+//
+//template class boost::ptr_unordered_map<int,T>;
 
-void test_const_element_container()
-{    
-    test_instantiations<const int>();
-    test_instantiations< boost::nullable<const int> >();
-    
-    template class
-    boost::ptr_set<const int>;
+// @todo: there seems to be some problems with
+//        argument passing in circular_buffer
+//
+//boost::ptr_circular_buffer<T> buffer(32);
+//buffer.push_back( new int(42)  );
 
-    // @todo: problem with constructor forwarding
-    //template class
-    //boost::ptr_unordered_set<T>;
+template class boost::ptr_set<const int>;
 
-}
-
-
-
-using boost::unit_test::test_suite;
-
-test_suite* init_unit_test_suite( int argc, char* argv[] )
-{
-    test_suite* test = BOOST_TEST_SUITE( "Pointer Container Test Suite" );
-
-    test->add( BOOST_TEST_CASE( &test_const_element_container ) );
-
-    return test;
-}
-
-
-
-
-
-
+// @todo: problem with constructor forwarding
+//
+//template class boost::ptr_unordered_set<T>;
